6503324e47136b7635230980841fe9e0d7388d3e
Jeremy Rempel <jdrempel@protonmail.com>
2022-06-16 21:42:46 -0600

    Use shortest rotation angle for player motion

--- characters/Player.gd
+++ characters/Player.gd
@@ -8,6 +8,7 @@ var velocity := Vector3.ZERO
 var target_location: Vector3
 var last_target_direction: Vector3
 var target_rotation: Basis
+var opposite_rotation: Basis
 var rotation_lerp := 0.0
 
 
@@ -16,19 +17,33 @@ func set_target_location(new_target: Vector3):
 	rotation_lerp = 0
 
 
-func rotate_player(delta):
-	if rotation_lerp < 1:
-		rotation_lerp += delta * turn_speed
-	elif rotation_lerp > 1:
-		rotation_lerp = 1
-	transform.basis = transform.basis.slerp(target_rotation, rotation_lerp).orthonormalized()
+func rotate_player(delta, target_direction):
+	target_rotation = transform.looking_at(target_location, Vector3.UP).basis
+	var target_quat = target_rotation.get_rotation_quat()
+	var angle_to_target = transform.basis.get_rotation_quat().angle_to(target_quat)
+	opposite_rotation = target_rotation.rotated(Vector3.UP, PI)
+	var opposite_quat = opposite_rotation.get_rotation_quat()
+	var angle_to_opposite = transform.basis.get_rotation_quat().angle_to(opposite_quat)
+	var pointing_vector: Vector3
+	if angle_to_target > angle_to_opposite:
+		target_rotation = opposite_rotation
+	var facing_vector := -transform.basis.z.normalized()
+	var opposing_vector := transform.basis.z.normalized()
+	if facing_vector.dot(target_direction) < 1 and opposing_vector.dot(target_direction) < 1:
+		if rotation_lerp < 1:
+			rotation_lerp += delta * turn_speed
+		elif rotation_lerp > 1:
+			rotation_lerp = 1
+		transform.basis = transform.basis.slerp(target_rotation, rotation_lerp).orthonormalized()
+	
+	return [facing_vector, opposing_vector]
 
 
 func destroy():
 	queue_free()
 
 
-func _process(delta):
+func get_movement_vector():
 	var target_direction := Vector3.ZERO
 	
 	if Input.is_action_pressed("move_up"):
@@ -40,16 +55,22 @@ func _process(delta):
 	if Input.is_action_pressed("move_right"):
 		target_direction.x += 1
 	
+	if target_direction.length():
+		return target_direction.normalized()
+	return target_direction
+
+
+func _process(delta):
+	
+	var target_direction = get_movement_vector()
 	if target_direction != Vector3.ZERO:
-		target_direction = target_direction.normalized()
 		var distance_to_target_location = (target_location - transform.origin).length()
-		if target_direction != last_target_direction or distance_to_target_location < 2 * move_speed * delta:
+		if target_direction != last_target_direction:
 			set_target_location(target_direction)
-		target_rotation = transform.looking_at(target_location, Vector3.UP).basis
-		rotate_player(delta)
-		var facing_vector := -transform.basis.z.normalized()
-		var opposing_vector := transform.basis.z.normalized()
-		if facing_vector.dot(target_direction) > 0.99 or opposing_vector.dot(target_direction) > 0.99:
+		var vectors = rotate_player(delta, target_direction)
+		var facing_vector = vectors[0]
+		var opposing_vector = vectors[1]
+		if facing_vector.dot(target_direction) > 0.999 or opposing_vector.dot(target_direction) > 0.999:
 			velocity = move_and_slide(move_speed * target_direction, Vector3.UP)
 	
 	last_target_direction = target_direction
