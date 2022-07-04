37beb838876696718b00e596625d50f03901f02e
Jeremy Rempel <jdrempel@protonmail.com>
2022-07-04 07:05:31 -0600

    Move player movement and turret rotation to remote

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -11,6 +11,11 @@ var target_rotation: Basis
 var opposite_rotation: Basis
 var rotation_lerp := 0.0
 
+# Network stuff
+puppet var p_origin := Vector3.ZERO
+puppet var p_basis := Basis.IDENTITY
+puppet var p_velocity := Vector3.ZERO
+
 
 signal destroyed()
 
@@ -79,7 +84,21 @@ func get_movement_vector():
 	return target_direction
 
 
+remote func update_pvr(pos, vel, rot):
+	p_origin = pos
+	p_basis = rot
+	p_velocity = vel
+
+
 func _physics_process(delta):
+	if not is_network_master():
+		# Player being controlled by remote source
+		global_transform.origin = p_origin
+		global_transform.basis = p_basis
+		velocity = p_velocity
+		return
+	
+	# Player being controlled by local client
 	var target_direction = get_movement_vector()
 	if target_direction != Vector3.ZERO:
 		if target_direction != last_target_direction:
@@ -93,3 +112,4 @@ func _physics_process(delta):
 		velocity = Vector3.ZERO
 	
 	last_target_direction = target_direction
+	rpc_unreliable("update_pvr", global_transform.origin, velocity, global_transform.basis)
--- characters/player/TurretRootPlayer.gd
+++ characters/player/TurretRootPlayer.gd
@@ -5,10 +5,26 @@ export(PackedScene) var ordnance
 onready var drop_plane = Plane(Vector3.UP, global_transform.origin.y)
 onready var camera = get_viewport().get_camera()
 
-func _process(delta):
-	var mouse_position = get_viewport().get_mouse_position()
-	var mouse_pos_3d = drop_plane.intersects_ray(
-		camera.project_ray_origin(mouse_position),
-		camera.project_ray_normal(mouse_position)
+var aim_pos_3d := Vector3.ZERO
+
+
+func _ready():
+	aim_pos_3d = drop_plane.intersects_ray(
+		Vector3.ZERO,
+		Vector3.UP
 	)
-	look_at(mouse_pos_3d, Vector3.UP)
+
+
+remote func update_aim(pos):
+	aim_pos_3d = pos
+
+
+func _process(delta):
+	if find_parent("Player*").is_network_master():
+		var mouse_position = get_viewport().get_mouse_position()
+		aim_pos_3d = drop_plane.intersects_ray(
+			camera.project_ray_origin(mouse_position),
+			camera.project_ray_normal(mouse_position)
+		)
+		rpc_unreliable("update_aim", aim_pos_3d)
+	look_at(aim_pos_3d, Vector3.UP)
