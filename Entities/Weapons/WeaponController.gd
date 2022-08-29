e783073c945bf0718c800d51838c934edbe18af8
Jeremy Rempel <jdrempel@protonmail.com>
2022-08-29 16:05:29 -0600

    Reorganize entire project structure

---
+++ Entities/Weapons/WeaponController.gd
@@ -0,0 +1,81 @@
+extends Node
+
+export(Array, PackedScene) var weapons: Array = []
+
+var _weapons: Array
+
+export var player_controlled = false
+
+var active_primary = null
+var active_secondary = null
+
+onready var primary_effect: CPUParticles = $"../Body/TurretRoot/FireCannonParticles"
+
+
+func has_active_primary() -> bool:
+    return active_primary != null
+
+
+func has_active_secondary() -> bool:
+    return active_secondary != null
+
+
+func set_active_primary_cooldown(cooldown: float):
+    if has_active_primary():
+        active_primary.cooldown_time = cooldown
+
+
+func set_active_secondary_cooldown(cooldown: float):
+    if has_active_secondary():
+        active_secondary.cooldown_time = cooldown
+
+
+func _ready():
+    for weapon in weapons:
+        var instance = weapon.instance()
+        add_child(instance)
+
+    for weapon in get_children():
+        if weapon.is_primary:
+            weapon.is_active = true
+            active_primary = weapon
+            break
+
+    for weapon in get_children():
+        if weapon.is_secondary:
+            weapon.is_active = true
+            active_secondary = weapon
+            break
+
+    for weapon in get_children():
+        if weapon.is_primary:
+            weapon.connect("fired", self, "on_primary_fired")
+        else:
+            weapon.connect("fired", self, "on_secondary_fired")
+
+
+remotesync func fire_primary():
+    if active_primary != null:
+        active_primary._fire()
+
+
+remotesync func on_primary_fired():
+    primary_effect.emitting = true
+
+
+remotesync func fire_secondary():
+    if active_secondary != null:
+        active_secondary._fire()
+
+
+remotesync func on_secondary_fired():
+    pass
+
+
+func _process(delta):
+    if player_controlled and is_network_master():
+        if Input.is_action_just_pressed("fire_primary"):
+            rpc("fire_primary")
+
+        if Input.is_action_just_pressed("fire_secondary"):
+            rpc("fire_secondary")
